f5c4a6ec1eef7bb111189a662d3d185c3c003536
Kevin Edward Dean <kevinedwarddean@gmail.com>
2025-10-19 00:45:15 -0400

    Update main.py
    fixed some code, changed a class name, etc.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import numpy as np
 
+'''
+* if needed on macOS, I ran into mutex errors, and the following commands help get passed it, 
+at least for me, might be different per user.
 import os
 os.environ["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
 os.environ["OMP_NUM_THREADS"] = "1"
 os.environ["MKL_NUM_THREADS"] = "1"
+'''
 
 from datetime import datetime, timedelta
 import warnings
@@ -17,12 +21,12 @@ from sentiment_analysis import *
 from merge_data import *
 from model import *
 
+COMPANY_NAME = "Apple"
 STOCK_SYMBOL = "AAPL"
 START_DATE = "2025-09-18"
 
 # get from https://newsapi.org/ subscription
 NEWS_API_KEY = "fde2f948fd9d43a396cba6d354e3e204"  
-COMPANY_NAME = "Apple"
 
 def main():
 	ld = Dataset(
@@ -52,16 +56,16 @@ def main():
 	md.merge_data()
 	df = md.df
 	
-	rmp = RegressionModelProphet(
+	prm = ProphetRegressionModel(
 		data=df, 
 		stock_symbol=STOCK_SYMBOL
 		)
-	rmp._fit()
-	rmp.update_future()
-	rmp.predict()
-	rmp._plot()
-	rmp._plot_components()
-	rmp.evaluate_rmse()
+	prm._fit()
+	prm.update_future()
+	prm.predict()
+	prm._plot()
+	prm._plot_components()
+	prm.evaluate_rmse()
 	
 	# Scale data
 	scaler = MinMaxScaler()
@@ -80,13 +84,14 @@ def main():
 	X_train, X_test = X[:train_size], X[train_size:]
 	y_train, y_test = y[:train_size], y[train_size:]
 	
-	data = {}
-	data['X'] = X
-	data['X_train'] = X_train
-	data['X_test'] = X_test
-	data['y_train'] = y_train
-	data['y_test'] = y_test
-	data['scaled'] = scaled
+	data = {
+		'X': X,
+		'X_train': X_train,
+		'X_test': X_test,
+		'y_train': y_train,
+		'y_test': y_test,
+		'scaled': scaled
+	}
 	
 	nnr = NeuralNetworkRegression(
 		scaler=scaler,
@@ -97,7 +102,6 @@ def main():
 	nnr.predict()
 	nnr.evaluate()
 	nnr.plot()
-	
 
 if __name__ == "__main__":
 	main()
